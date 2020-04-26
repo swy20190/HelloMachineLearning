c8d148a3057a985422db8b37b978ecf86f9694a9
swy20190 <K201410109@163.com>
2020-04-26 21:56:23 +0800

    add the depth=8 tree for 2nd set

--- codes/helloMachineLearning.py
+++ codes/helloMachineLearning.py
@@ -43,6 +43,7 @@ graph = graphviz.Source(treeAB_d8_dot)
 graph.render("../output/TreeForAgeAndBalanceD8")
 
 # second set of duration, campaign, pdays, as well as previous
+# performs much more better than the 1st one
 second_set = bank_data[['duration', 'campaign', 'pdays', 'previous']]
 # decision tree max_depth=4
 clf_treeDrtCpnPdsPrev_d4 = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
@@ -61,6 +62,22 @@ treeDrtCpnPdsPrev_d4_dot = tree.export_graphviz(
 )
 graph = graphviz.Source(treeDrtCpnPdsPrev_d4_dot)
 graph.render("../output/TreeForDrtCpnPdsPrevD4")
+# decision tree max_depth=8
+clf_treeDrtCpnPdsPrev_d8 = tree.DecisionTreeClassifier(criterion="entropy", max_depth=8)
+clf_treeDrtCpnPdsPrev_d8 = clf_treeDrtCpnPdsPrev_d8.fit(second_set, labels)
+scores_treeDrtCpnPdsPrev_d8 = cross_val_score(
+    clf_treeDrtCpnPdsPrev_d8, second_set, labels, cv=10
+) # 10-means cross validate
+print(scores_treeDrtCpnPdsPrev_d8)
+# visualize
+treeDrtCpnPdsPrev_d8_dot = tree.export_graphviz(
+    clf_treeDrtCpnPdsPrev_d8
+    , out_file=None
+    , feature_names=feature_name_2
+    , class_names=class_name
+)
+graph = graphviz.Source(treeDrtCpnPdsPrev_d8_dot)
+graph.render("../output/TreeForDrtCpnPdsPrevD8")
 
 # write scores into file
 with open("../output/scoresOfTrees.txt", "w") as scoreFile:
@@ -72,4 +89,6 @@ with open("../output/scoresOfTrees.txt", "w") as scoreFile:
     scoreFile.write("\n")
     scoreFile.write("Scores of TreeForDrtCpnPdsPrev, depth=4\n")
     scoreFile.write(str(scores_treeDrtCpnPdsPrev_d4))
+    scoreFile.write("\n")
+    scoreFile.write("Scores of TreeForDrtCpnPdsPrev, depth=8\n")
     scoreFile.write("\n")
