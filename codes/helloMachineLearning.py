2f4db8dc68ea735519afe08a203b2b13360d79ef
swy20190 <K201410109@163.com>
2020-04-26 17:51:14 +0800

    another tree with maxDepth = 8

--- codes/helloMachineLearning.py
+++ codes/helloMachineLearning.py
@@ -9,23 +9,42 @@ first_set = bank_data[['balance', 'duration']]
 labels = bank_data['y']
 # print(first_set)
 # decision tree begins
-clf = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
-clf = clf.fit(first_set, labels)
-scores_treeBD_d4 = cross_val_score(clf, first_set, labels, cv=10)  # 10-means cross validate
+# decision tree max_depth=4
+clf_treeBD_d4 = tree.DecisionTreeClassifier(criterion="entropy", max_depth=4)
+clf_treeBD_d4 = clf_treeBD_d4.fit(first_set, labels)
+scores_treeBD_d4 = cross_val_score(clf_treeBD_d4, first_set, labels, cv=10)  # 10-means cross validate
 print(scores_treeBD_d4)
 # visualize
 feature_name = ['balance', 'duration']
-firstTree_dot = tree.export_graphviz(
-    clf
+class_name = ['not_buy','buy']
+treeBD_d4_dot = tree.export_graphviz(
+    clf_treeBD_d4
     , out_file=None
     , feature_names=feature_name
-    , class_names=['not_buy', 'buy']
+    , class_names=class_name
 )
-graph = graphviz.Source(firstTree_dot)
-graph.render("../output/TreeForBalanceAndDuration")
+graph = graphviz.Source(treeBD_d4_dot)
+graph.render("../output/TreeForBalanceAndDurationD4")
+# decision tree max_depth=8
+clf_treeBD_d8 = tree.DecisionTreeClassifier(criterion="entropy", max_depth=8)
+clf_treeBD_d8 = clf_treeBD_d8.fit(first_set, labels)
+scores_treeBD_d8 = cross_val_score(clf_treeBD_d8, first_set, labels, cv=10)  # 10-means cross validate
+print(scores_treeBD_d8)
+# visualize
+treeBD_d8_dot = tree.export_graphviz(
+    clf_treeBD_d8
+    , out_file=None
+    , feature_names=feature_name
+    , class_names=class_name
+)
+graph = graphviz.Source(treeBD_d8_dot)
+graph.render("../output/TreeForBalanceAndDurationD8")
 
 # write scores into file
 with open("../output/scoresOfTrees.txt", "w") as scoreFile:
     scoreFile.write("Scores of TreeForBalanceAndDuration, depth=4\n")
     scoreFile.write(str(scores_treeBD_d4))
+    scoreFile.write("\n")
+    scoreFile.write("Scores of TreeForBalanceAndDuration, depth=8\n")
+    scoreFile.write(str(scores_treeBD_d8))
     scoreFile.write("\n")
