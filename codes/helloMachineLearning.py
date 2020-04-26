a4ee202189ae147720795f64b1388cd9c30497cb
swy20190 <K201410109@163.com>
2020-04-26 14:26:58 +0800

    import anaconda

--- codes/helloMachineLearning.py
+++ codes/helloMachineLearning.py
@@ -1,6 +1,13 @@
 import numpy as np
+from sklearn import tree
 
-
-first_set = np.loadtxt("../datas/train_set.csv", str, delimiter=",", usecols=(6, 12), skiprows=-1)  # balance & duration
-print(first_set)
-
+first_set = np.loadtxt(
+    "../datas/train_set.csv", str, delimiter=",", usecols=(17, 6, 12), skiprows=1
+)  # balance & duration
+first_set_label = np.loadtxt(
+    "../datas/train_set.csv", str, delimiter=",", usecols=17, skiprows=1
+)  # 1st labels
+# print(first_set)
+# decision tree begins
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(first_set, first_set_label)
