b3e02879ec1bd0dbd16b2c96774702f54ce6a5e1
swy20190 <K201410109@163.com>
2020-04-26 17:20:37 +0800

    10-means cross validate and visualize the decision tree

--- codes/helloMachineLearning.py
+++ codes/helloMachineLearning.py
@@ -1,13 +1,26 @@
 import numpy as np
+import pandas as pd
 from sklearn import tree
+import graphviz
+from sklearn.model_selection import cross_val_score
 
-first_set = np.loadtxt(
-    "../datas/train_set.csv", str, delimiter=",", usecols=(17, 6, 12), skiprows=1
-)  # balance & duration
-first_set_label = np.loadtxt(
-    "../datas/train_set.csv", str, delimiter=",", usecols=17, skiprows=1
-)  # 1st labels
+bank_data = pd.read_csv("../datas/train_set.csv")
+first_set = bank_data[['balance', 'duration']]
+labels = bank_data['y']
 # print(first_set)
 # decision tree begins
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(first_set, first_set_label)
+clf = clf.fit(first_set, labels)
+scores = cross_val_score(clf, first_set, labels, cv=10)  # 10-means cross validate
+print(scores)
+feature_name = ['balance', 'duration']
+
+firstTree_dot = tree.export_graphviz(
+    clf
+    , out_file=None
+    , feature_names=feature_name
+    , class_names=['not_buy', 'buy']
+)
+graph = graphviz.Source(firstTree_dot)
+graph.render("../output/TreeForBalanceAndDuration")
+
