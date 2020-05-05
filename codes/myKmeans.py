292f5fec0a7a72c2298adc1142c865e6499ecb78
swy20190 <K201410109@163.com>
2020-05-05 11:44:15 +0800

    rand centroids

--- codes/myKmeans.py
+++ codes/myKmeans.py
@@ -2,14 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+def distEclud(vec_a, vec_b):
+    return sum((vec_a - vec_b)**2)**0.5
+
+
+def randCent(dataset, k):
+    n = np.shape(dataset)[1]
+    centroids = np.mat(np.zeros([k, n]))
+    for i in range(n):
+        maxi = max(dataset[:, i])
+        mini = min(dataset[:, i])
+        centroids[:, i] = mini + (maxi - mini) * np.random.random([k, 1])
+    return centroids
+
 frog_data = pd.read_csv("../datas/Frogs_MFCCs.csv")
-mfcc_1 = frog_data['MFCCs_ 1']
-mfcc_5 = frog_data['MFCCs_ 5']
-mfcc_9 = frog_data['MFCCs_ 9']
-mfcc_13 = frog_data['MFCCs_13']
-mfcc_17 = frog_data['MFCCs_17']
-mfcc_21 = frog_data['MFCCs_21']
-first_set = []
-for i in range(0, len(mfcc_1)):
-    first_set.append([mfcc_1[i], mfcc_5[i], mfcc_9[i], mfcc_13[i], mfcc_17[i], mfcc_21[i]])
-print(first_set)
+first_set = frog_data[['MFCCs_ 1', 'MFCCs_ 5', 'MFCCs_ 9', 'MFCCs_13', 'MFCCs_17', 'MFCCs_21']]
+# print(first_set)
+first_set = first_set.values
+# print(first_set)
+jj = randCent(first_set, 4)
+print(jj)
